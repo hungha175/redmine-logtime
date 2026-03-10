f4ad692bb86aa9ecfe83b450f746d5977b2989b4
Hung Ha <hung.ha@gotit.vn>
2026-03-10 15:43:44 +0700

    Fix 500: add TrustProxies and Force HTTPS for Render

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->web(prepend: [\App\Http\Middleware\SanitizeApiKey::class], append: [SecurityHeaders::class]);
         $middleware->alias(['app_basic_auth' => AppBasicAuth::class]);
     })
